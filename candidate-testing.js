fb3fa274bcae0bc78e7dfae3aa84d5ce1e877717
Darrian Miller <>
2021-06-04 21:20:16 +0000

    assigned global variables no values.

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,12 +4,12 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 
-let candidateName = "";
+let candidateName;
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
