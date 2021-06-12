c6c26f419ff48b4deb5f7a19e594594d930eee1b
Darrian Miller <35920220+NinoGotti5@users.noreply.github.com>
2021-06-12 23:51:55 +0000

    Part 2 Complete.

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,41 +10,46 @@ let candidateName = "";
 let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = [
+const questions = [
   "Who was the first American woman in space? ",
-  "True or false: 5 kilometer == 5000 meters? ", 
+  "True or False: 5 kilometer == 5000 meters? ", 
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "
   ];
-let correctAnswers = [
+const correctAnswers = [
   "Sally Ride",
-  "true",
+  "True",
   "40",
   "Trajectory",
   "3"
   ];
-let candidateAnswers;
+let candidateAnswers = [];
 
 
 function askForName() {
-  candidateName = input.question("What is your name: ");
-  console.log("Hi, " + candidateName + "!");
+  candidateName = input.question("Candidate Name: ");
+  console.log(`Hi, ${candidateName}!`);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer
-  candidateAnswer = input.question("Who was the first American woman in space: ");
+  for (let i = 0; i < questions.length; i++){
+    candidateAnswers[i] = input.question(`${questions[i]} \nYour answer: `);
+    console.log(`CorrectAnswer: ${correctAnswers[i]} \n`);
 }
+
+
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-  if (candidateAnswer === "Sally Ride") {
+  if (candidateAnswer.toUpperCase() === correctAnswers.toUpperCase() ) {
     console.log("Your answer is correct!");
   } else {
     console.log('Your answer is incorrect! Try again!')
   }
+}
+  
 
 
   let grade;
@@ -72,6 +77,6 @@ module.exports = {
   questions: questions,
   correctAnswers: correctAnswers,
   candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
+  //gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
