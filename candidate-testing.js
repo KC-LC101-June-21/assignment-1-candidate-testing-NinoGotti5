929329180343df769a5eac39052258b861f62d43
Darrian Miller <35920220+NinoGotti5@users.noreply.github.com>
2021-06-16 02:16:38 +0000

    Removed space in question

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,7 +7,7 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 const questions = [ "Who was the first American woman in space? ",
